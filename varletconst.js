f3036509e1f8ff416f74cec288b462168eb2895b
unknown <jaym991776@gmail.com>
2026-07-24 23:48:35 -0400

    var,let,const advanced

--- varletconst.js
+++ varletconst.js
@@ -1,11 +1,5 @@
-let a = 100;
-a = 200; //재할당 Reassignment O
-//let a = 500; //재선언 Redeclaration X
-//SyntaxError: Identifier 'a' has already been declared
-console.log(a);
-
-var b = 300;
-
-var b = 400;
-b = 700;
-console.log(b);
+for (let i = 1; i <= 3; i++) {
+  setTimeout(function () {
+    console.log("A:", i);
+  }, 1000 * i);
+}
