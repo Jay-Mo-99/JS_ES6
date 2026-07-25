67ffa5dfda676ac21e5c6cca903c943edc81e939
unknown <jaym991776@gmail.com>
2026-07-25 02:38:56 -0400

    var,let,const advanced

--- varletconst.js
+++ varletconst.js
@@ -1,5 +1,6 @@
-for (let i = 1; i <= 3; i++) {
-  setTimeout(function () {
-    console.log("A:", i);
-  }, 1000 * i);
+for (var i = 1; i <= 3; i++) {
+  setTimeout(() => console.log("X:", i), 1000 * i);
+}
+for (let j = 1; j <= 3; j++) {
+  setTimeout(() => console.log("Y:", j), 1000 * j);
 }
