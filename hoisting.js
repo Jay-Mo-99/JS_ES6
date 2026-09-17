77b5e45220c2cf491f02d0e41908d8c0fc6c10e3
unknown <jaym991776@gmail.com>
2026-09-17 02:33:05 -0400

    Explain the hoisting and async with function

--- hoisting.js
+++ hoisting.js
@@ -1,13 +1,17 @@
-console.log(test2); //ReferenceError: test2 is not defined
-console.log(test);
-console.log(multiplyAB);
+// console.log(test2); //ReferenceError: test2 is not defined
+// console.log(test);
+// console.log(multiplyAB);
 
-function multiplyAB(a, b) {
-  return a * b;
+// function multiplyAB(a, b) {
+//   return a * b;
+// }
+// var test = "Test"; //underined
+
+// //let test = "Test";
+// //ReferenceError: Cannot access 'test' before initialization
+// //const test = "Test";
+// //ReferenceError: Cannot access 'test' before initialization
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 0);
 }
-var test = "Test"; //underined
-
-//let test = "Test";
-//ReferenceError: Cannot access 'test' before initialization
-//const test = "Test";
-//ReferenceError: Cannot access 'test' before initialization
